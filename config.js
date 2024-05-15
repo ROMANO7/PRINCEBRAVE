7a823f0fb790074b5d77d0ab4498a9e54da18f7e
ROMANO7 <89288883+ROMANO7@users.noreply.github.com>
2024-05-15 02:00:42 -0500

    15 DE MAYO

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
-import { watchFile, unwatchFile } from 'fs'
-import chalk from 'chalk'
-import { fileURLToPath } from 'url'
-import fs from 'fs'
-import fetch from 'node-fetch'
-import axios from 'axios'
+import { watchFile, unwatchFile } from "fs"
+import chalk from "chalk"
+import { fileURLToPath } from "url"
+import fs from "fs"
+import cheerio from "cheerio"
+import fetch from "node-fetch"
+import axios from "axios"
+import moment from "moment-timezone"
+
+
+
+
 
 global.owner = [ 
 ['6283123884674', 'BRAVEHEART', false], 
@@ -11,10 +17,12 @@ global.owner = [
 ['19472302626'],
 ['351923250010'],
 ['34691926968']
+ 
+]
+//💌------------------------------------------💌
 
-]; //Number of owners
 
-//global.pairingNumber = "6283123884674" //put your bot number here
+//💌global.pairingNumber = "6283123884674" //put your bot number here💌
 global.mods = ['79271041971'] 
 global.prems = ['351923250010', '19472302626', '34691926968']
 global.allowed = ['79271041971']
@@ -25,20 +33,102 @@ global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
 global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
 global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
 global.lolkeysapi = ['GataDios']
+//💌------------------------------------------💌
 
+//💌------------------------------------------💌
+//CONFIG VARS. Do not touch them⚠️
+
+  global.autoread = process.env.AUTOREAD !== 'false',
+  global.statusView = process.env.STATUSVIEW !== 'false',
+  global.autoreaction = process.env.AutoReaction !== 'false',
+
+//💌------------------------------------------💌
+
+    
+// APIS
 global.APIs = { // API Prefix
   // name: 'https://website'
+  CFROSAPI: 'https://api.cafirexos.com',
   xteam: 'https://api.xteam.xyz', 
   nrtm: 'https://fg-nrtm.ddns.net',
   bg: 'http://bochil.ddns.net',
   fgmods: 'https://api.fgmods.xyz'
 }
+// 💌------------------------------------------💌
+
+
+
+
+//APIKEYS
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
   'https://api.xteam.xyz': 'd90a9e986e18778b',
   'https://zenzapis.xyz': '675e34de8a', 
   'https://api.fgmods.xyz': 'dEBWvxCY'
 }
+//💌------------------------------------------💌
+
+
+
+// Bot Images 
+global.imagen1 = fs.readFileSync("./Assets/menus/Menu.png")
+global.imagen2 = fs.readFileSync("./Assets/menus/Menu1.jpg")
+global.imagen3 = fs.readFileSync("./Assets/menus/Menu2.jpg")
+global.imagen4 = fs.readFileSync("./Assets/menus/Menu3.jpg")
+global.imagen5 = fs.readFileSync("./Assets/menus/img2.jpg")
+global.imagen6 = fs.readFileSync("./Assets/menus/img5.jpg")
+global.imagen7 = fs.readFileSync("./Assets/menus/img6.jpg")
+global.imagen8 = fs.readFileSync("./Assets/menus/img8.jpg")
+global.imagen9 = fs.readFileSync("./Assets/menus/img9.jpg")
+global.imagen10 = fs.readFileSync("./Assets/menus/img11.jpg")
+global.imagen11 = fs.readFileSync("./Assets/menus/img12.jpg")
+//💌------------------------------------------💌
+
+
+
+global.img = 'https://i.imgur.com/IXlUwTW.jpg'
+global.img2 = 'https://i.imgur.com/EXTbyyn.jpg'
+global.img3 = 'https://i.imgur.com/oUAGYc2.jpg' 
+global.img4 = 'https://i.imgur.com/i0pccuo.jpg' 
+global.img5 = 'https://i.imgur.com/iL1snRx.jpeg'
+global.img6 = 'https://i.imgur.com/cYFgSKv.jpeg'
+global.img7 = 'https://i.imgur.com/JqL3h2V.jpeg'
+global.img8 = 'https://i.imgur.com/PCujt1s.jpeg'
+global.img9 = 'https://i.imgur.com/xfUEdDb.jpeg'
+global.img10 = 'https://i.imgur.com/DvHoMc3.jpg'
+global.img11 = 'https://i.imgur.com/5Q1MqGD.jpg'
+global.img12 = 'https://i.imgur.com/vWnsjh8.jpg'
+global.img13 = 'https://i.imgur.com/pCfFOgw.jpeg'
+global.img14 = 'https://i.imgur.com/knBDWRA.jpeg'
+global.img15 = 'https://i.imgur.com/QrkkKx7.jpeg'
+global.img16 = 'https://i.imgur.com/JpYfcH0.jpeg'
+global.img17 = 'https://i.imgur.com/9yLH4W4.jpeg'
+//💌------------------------------------------💌
+
+
+//Chatgpt
+global.gpt1 = fs.readFileSync("./Assets/GPT/gpt1.jpg")
+global.gpt2 = fs.readFileSync("./Assets/GPT/gpt2.png")
+global.gpt3 = fs.readFileSync("./Assets/GPT/gpt3.png")
+global.gpt4 = fs.readFileSync("./Assets/GPT/gpt4.png")
+global.gpt5 = fs.readFileSync("./Assets/GPT/gpt5.png")
+global.gpt6 = fs.readFileSync("./Assets/GPT/gpt6.png")
+//💌------------------------------------------💌
+
+
+// Randome
+global.princeMenu = [img, img2, img6, img7, img8, img9, img13, img14, img15, img16, img17]
+global.princeImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11]
+global.princegpt = [gpt1, gpt2, gpt3, gpt4, gpt5, gpt6]
+//💌------------------------------------------💌
+
+
+
+// Moderator 
+global.developer = 'https://wa.me/message/DCAK67ON3XVOG1' //contact
+//💌------------------------------------------💌
+
+
 
 // Sticker WM
 global.botname = 'BRAVEHEART'
@@ -52,7 +142,11 @@ global.Princesc = 'https://github.com/PRINCE-GDS/THE-PRINCE-BOT'
 global.princeyt = 'https://youtube.com/'
 global.Princelog = 'https://i.imgur.com/h7FzZNH.jpeg'
 global.thumb = fs.readFileSync('./Assets/Prince.png')
+//💌------------------------------------------💌
 
+
+
+//Reactions
 global.wait = '*♻️ _𝙶𝙴𝚃𝚃𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 𝙵𝙸𝙻𝙴 𝚆𝙰𝙸𝚃..._*\n*▰▰▰▱▱▱▱▱*'
 global.imgs = '*🖼️ _𝙶𝙴𝚃𝚃𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 ɪᴍᴀɢᴇs 𝚆𝙰𝙸𝚃..._*\n*▰▰▰▱▱▱▱▱*'
 global.rwait = '♻️'
@@ -60,9 +154,14 @@ global.dmoji = '🤭'
 global.done = '✅'
 global.error = '❌' 
 global.xmoji = '🌀' 
-
 global.multiplier = 69 
-global.maxwarn = '2' // máxima advertencias
+global.maxwarn = '2' 
+//💌------------------------------------------💌
+
+
+
+
+
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
